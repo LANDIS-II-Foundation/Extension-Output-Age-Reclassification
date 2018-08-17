583999418c95f31da04bc2d3695077fd910c3f45
Robert Scheller <rschell@ncsu.edu>
2018-08-17 09:10:49 -0400

    Update installer and examples.

--- testings/branch-tests/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyDescription("Reclass - Output Plug-in")]
-[assembly: AssemblyProduct("Landis-II")]
-[assembly: AssemblyCompany("Portland State University")]
-[assembly: AssemblyCopyright("Copyright 2010 Portland State University")]
